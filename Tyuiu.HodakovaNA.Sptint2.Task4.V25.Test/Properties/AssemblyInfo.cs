4e48a85e90a89840e492138cf5ba8b4da6b9674b
HodakovaNA <144614378+HodakovaNA@users.noreply.github.com>
2023-10-10 23:45:11 +0500

    Task4

---
+++ Tyuiu.HodakovaNA.Sptint2.Task4.V25.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.HodakovaNA.Sptint2.Task4.V25.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.HodakovaNA.Sptint2.Task4.V25.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2fa5aa66-cf7e-4892-b7cf-69d43c48e5e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.HodakovaNA.Sptint2.Task4.V25/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.HodakovaNA.Sptint2.Task4.V25.Lib;
+
+namespace Tyuiu.HodakovaNA.Sprint2.Task4.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнила Ходакова Н.А. | АСОиУб-23-1";
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                                       *");
+            Console.WriteLine("* Тема: Базовые навыки работы с C#                                                                *");
+            Console.WriteLine("* Задание #4                                                                                      *");
+            Console.WriteLine("* Вариант #25                                                                                     *");
+            Console.WriteLine("* Выполнила: Ходакова Надежда Антоновна | АСОиУб-23-1                                             *");
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                *");
+            Console.WriteLine("***************************************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X:");
+            double x = Convert.ToDouble(Console.ReadLine()); 
+            
+            Console.WriteLine("Введите значение переменной Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                      *");
+            Console.WriteLine("***************************************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+
+        }
+    }
+}
